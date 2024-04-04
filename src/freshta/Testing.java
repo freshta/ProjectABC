a22d7e7ef312f08a9d5414cc5656c2a52906f21c
Freshta <freshta.habibzay@gmail.com>
2024-04-04 07:06:32 -0400

    New Project

---
+++ src/freshta/Testing.java
@@ -0,0 +1,11 @@
+package freshta;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello my name is freshta, and I am testing this functionality");
+	}
+
+}
